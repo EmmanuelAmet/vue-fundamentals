1d6298a83629e8857841f78f7550d534a8f24817
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2021-12-14 14:00:15 -0800

    Modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,6 @@
 <template>
 <!-- Form Handling -->
- <form @submit.prevent="submitForm">
-   <div>
-     <pre>
-       {{JSON.stringify(formValues, null , 2)}}
-     </pre>
-   </div>
-   <div>
-     <label for="name">Name</label>
-     <input type="text" id="name" v-model.trim.lazy="formValues.name">
-   </div>
-   <div>
-     <label for="profile">Profile Summary</label>
-     <textarea id="profile" v-model.trim.lazy="formValues.profileSummary"></textarea>
-   </div>
-   <div>
-     <label for="country">Country</label>
-     <select id="country" v-model="formValues.country">
-       <option value="">Select Country</option>
-       <option value="ghana">Ghana</option>
-       <option value="germany">Germany</option>
-       <option value="canada">Canada</option>
-     </select>
-   </div>
-   <div>
-     <label for="job-location">Job Location</label>
-     <select id="country" multiple v-model="formValues.jobLocation">
-       <option value="">Select Country</option>
-       <option value="ghana">Ghana</option>
-       <option value="germany">Germany</option>
-       <option value="canada">Canada</option>
-     </select>
-   </div>
-   <div>
-     <input type="checkbox" id="remotework" true-value="yes" false-value="no" v-model="formValues.remoteWork">
-     <label for="remotework">Open to remote work</label>
-   </div>
-   <div>
-     <label>Skill set</label>
-     <input type="checkbox" id="html" value="html" v-model="formValues.skillSet">
-     <label for="html">HTML</label>
-     <input type="checkbox" id="css" value="css" v-model="formValues.skillSet">
-     <label for="css">CSS</label>
-     <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet">
-     <label for="javascript">javascript</label>
-   </div>
-   <div>
-     <label>Years of Experience</label>
-     <input type="radio" id="0-2" value="0-2" v-model="formValues.yearsOfExperience">
-     <label for="0-2">0-2</label>
-     <input type="radio" id="3-5" value="3-5" v-model="formValues.yearsOfExperience">
-     <label for="3-5">3-5</label>
-     <input type="radio" id="6-10" value="6-10" v-model="formValues.yearsOfExperience">
-     <label for="6-10">6-10</label>
-     <input type="radio" id="10+" value="10+" v-model="formValues.yearsOfExperience">
-     <label for="10+">10+</label>
-   </div>
-   <div>
-    <label for="age">Age</label>
-     <input @keyup.enter="submitForm" type="number" id="age" v-model.number="formValues.age">
-   </div>
-   <div>
-     <button>Submit</button>
-   </div>
- </form>
+ 
 </template>
 
 <script>
@@ -72,22 +9,11 @@ export default {
   name: 'App', 
   data () {
     return{
-      formValues: {
-        name: '',
-        profileSummary: '',
-        country: '',
-        jobLocation: [],
-        remoteWork: 'no',
-        skillSet: [],
-        yearsOfExperience: '',
-        age: null
-      }
+
     }
   },
   methods: {
-    submitForm(){
-      console.log('Form values', this.formValues)
-    }
+    
   }
 }
 </script>
