e1e2bfce25091e0fc4af9d238d27183cf9b2964b
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2021-12-15 20:18:01 -0800

    Dynamic Components

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
-  <h4>App Component Text</h4>
-  <ChildStyles>
-    <h4>Child Style</h4>
-  </ChildStyles>
+  <button @click="activeTab = 'TabA'">Tab A</button>
+  <button @click="activeTab = 'TabB'">Tab B</button>
+  <button @click="activeTab = 'TabC'">Tab C</button>
+
+<keep-alive>
+<component :is="activeTab" />
+</keep-alive>
+  <!-- <TabA v-if="activeTab === 'TabA'" />
+  <TabB v-if="activeTab === 'TabB'" />
+  <TabC v-if="activeTab === 'TabC'" /> -->
 </template>
 
 <script>
-import ChildStyles from './components/ChildStyles.vue'
+import ComponentC from './components/ComponentC.vue'
+import TabA from './components/Tabs/TabA.vue'
+import TabB from './components/Tabs/TabB.vue'
+import TabC from './components/Tabs/TabC.vue'
 
 export default {
   name: 'App', 
   components: {
-    ChildStyles,
+    TabA,
+    TabB,
+    TabC,
+    ComponentC,
   },
   data () {
     return {
-
+      activeTab: 'TabA'
     }
   },
   methods: {}
---
+++ src/components/Tabs/TabC.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Tab C Content
+    </div>
+    <keep-alive>
+    <input type="text" v-model="name">
+    </keep-alive>
+</template>
+
+<script>
+    export default {
+        name: 'TabC',
+        data(){ 
+            return {
+                name: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
