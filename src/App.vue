c01e69266fc1e863fb26d431fdc5f42c7003f461
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2021-12-15 18:23:57 -0800

    Components Events

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,21 @@
 <template>
-  <!-- <Greet name="Manuel" hero-name="Walls"/>
-  <Greet name="Diana" hero-name="Flash"/>
-  <Greet :name="name" :hero-name="channel"/> -->
-  <!-- <Article id="my-article" title="Title" :likes="38" :isPublished="true"/> -->
-
-  <!-- <h2>{{name}}</h2>
-  <ComponentC/>
-  <ComponentE />
-  <ComponentF /> -->
-
-  <button @click="showPopup = true">Show Popup</button>
-  <Popup v-show="showPopup" @close="closePopup"/>
+  <Input v-model="name"/>
 </template>
 
 <script>
-
-import Greet from './components/Greet.vue'
-import Article from './components/Article.vue'
-import ComponentC from './components/ComponentC.vue'
-import ComponentE from './components/ComponentE.vue'
-import ComponentF from './components/ComponentF.vue'
-import Popup from './components/Popup.vue'
+import Input from './components/Input.vue'
 
 export default {
   name: 'App', 
   components: {
-    Greet,
-    Article,
-    ComponentC,
-    ComponentE,
-    ComponentF,
-    Popup,
+    Input,
   },
   data () {
     return {
-      name: 'ManuelAmet',
-      channel: 'CodeManuel',
-      showPopup: false,
+      name: '',
     }
   },
-  methods: {
-    closePopup(name){
-      this.showPopup = false
-      console.log('name', name)
-    }
-  },
-  provide() {
-    return {
-      username: this.name}
-  }
+  methods: {}
 }
 </script>
 
---
+++ src/components/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- <Greet name="Manuel" hero-name="Walls"/>
+  <Greet name="Diana" hero-name="Flash"/>
+  <Greet :name="name" :hero-name="channel"/> -->
+  <!-- <Article id="my-article" title="Title" :likes="38" :isPublished="true"/> -->
+
+  <!-- <h2>{{name}}</h2>
+  <ComponentC/>
+  <ComponentE />
+  <ComponentF /> -->
+
+  <button @click="showPopup = true">Show Popup</button>
+  <Popup v-show="showPopup" @close="closePopup"/>
+</template>
+
+<script>
+
+import Greet from './components/Greet.vue'
+import Article from './components/Article.vue'
+import ComponentC from './components/ComponentC.vue'
+import ComponentE from './components/ComponentE.vue'
+import ComponentF from './components/ComponentF.vue'
+import Popup from './components/Popup.vue'
+
+export default {
+  name: 'App', 
+  components: {
+    Greet,
+    Article,
+    ComponentC,
+    ComponentE,
+    ComponentF,
+    Popup,
+  },
+  data () {
+    return {
+      name: 'ManuelAmet',
+      channel: 'CodeManuel',
+      showPopup: false,
+    }
+  },
+  methods: {
+    closePopup(name){
+      this.showPopup = false
+      console.log('name', name)
+    }
+  },
+  provide() {
+    return {
+      username: this.name}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
